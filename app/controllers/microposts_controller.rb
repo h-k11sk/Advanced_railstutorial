27492f907fe25de9248f9a1dfba9df66e9635eba
Keisuke Hara <hara.keisuke@itolab.nitech.ac.jp>
2014-10-17 11:51:14 +0900

    Add ajax in feed-post

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,14 +2,20 @@ class MicropostsController < ApplicationController
   before_action :signed_in_user, only:[:create, :destroy]
   before_action :correct_user, only: :destroy
 
-  def index
 
+  def index
   end
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
+
+      @feed_items = current_user.feed.paginate(page: params[:page])
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      respond_to do |format|
+        format.html {redirect_to root_url}
+        format.js
+      end
+#      redirect_to root_url
     else
       @feed_items = []
       render 'static_pages/home'
